a1a57e7e5980bd0fed9eff7422bdf99e923cd8de
Cefan Rubin <cefan@cloudflare.com>
2021-09-17 07:11:02 +0100

    fix path

--- _util/download_blog_rss.sh
+++ _util/download_blog_rss.sh
@@ -3,7 +3,12 @@
 SCRIPT_DIR=$(dirname $(pwd -P $0)/${0#\.\/})
 pushd $SCRIPT_DIR > /dev/null
 
+echo $PWD
+echo $PWD
+echo $PWD
+
 curl 'https://blog.cloudflare.com/tag/research/rss/' -o ../rss.xml
 
+ls -l ../rss.xml
 
 popd > /dev/null
