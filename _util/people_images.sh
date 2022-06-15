b2b604cfb1edb80f6a79c102cdea03707c0d0eb6
Cefan Rubin <cefan@cloudflare.com>
2022-06-15 14:09:13 -0400

    catchup images script here too

--- _util/people_images.sh
+++ _util/people_images.sh
@@ -13,22 +13,22 @@ PROFILE_PATHS="$PEOPLE_PATHS $INTERN_PATHS $RESEARCHER_PATHS"
 # make sure that we have a picture for all people
 for path in $PROFILE_PATHS; do
 
-  echo $path
+  #echo $path
   person=$(basename "$path" .md)
-  echo $person
+  #echo $person
   image_path="../img/people/${person}.jpg"
-  echo $image_path
+  #echo $image_path
 
   # if the image does not exist and this .md file contains a 'position' (is a profile)
   if [ ! -f "${image_path}" ] && grep -q 'position' "$path"; then
     echo "No image found for: $person"
-    echo "Looking for an original image at: ${image_path}.original"
+    echo "Looking for an original image at: ${image_path:3}.original"
 
     if [ -f "${image_path}.original" ]; then
-      echo "Converting '${image_path}.jpg.original'"
+      echo "Converting '${image_path:3}.jpg.original'"
       convert ${image_path}.original -resize '400X400^' -gravity center -extent '400x400' ${image_path}
     else
-      echo "'${image_path}.original not found. Please add and retry."
+      echo "'${image_path:3}.original not found. Please add and retry."
       echo
       exit 1
     fi
